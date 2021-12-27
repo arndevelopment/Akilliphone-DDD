c8ac29e6bf23862d4a96634e2cc7083598fdd105
ARN DEVELOPMENT <93433859+arndevelopment@users.noreply.github.com>
2021-12-27 17:18:51 +0300

    Update

---
+++ Order.Domain/AggregateModels/OrderModels/Adress.cs
@@ -0,0 +1,24 @@
+﻿using Order.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.AggregateModels.OrderModels
+{
+    public class Adress : ValueObject
+    {
+        //Property tanımlamaları bu şekilde yapılıyor 
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            //Tanımlanan propertyler bu şekilde kontrole sokuluyor
+            yield return City;
+            yield return Country;
+        }
+    }
+}
